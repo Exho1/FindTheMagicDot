5c491b30380d9c245061e8e8519bb9da0bd9d24e
GraysonHerbert <65082415+GraysonHerbert@users.noreply.github.com>
2020-10-29 14:52:23 -0500

    Added comments to the code.

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
-    float speed = 500f;
+    float movement_speed = 500f;
     float rotation_speed = 40f;
-    Rigidbody rb;
-    float current_angle;
-    public GameObject guide_view;
+    Rigidbody rb; //Holds the seeker object
+    float current_angle; //The current angle the Seeker is facing from the negative x axis
+    public GameObject guide_view; //Holds the guide camera
 
     // Start is called before the first frame update
     void Start()
@@ -23,31 +23,38 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Multiplier for the speed in the x direciton
         float xDir = (float)Math.Cos(current_angle * Math.PI / 180);
+        //Multiplier for the speed in the z direction
         float zDir = (float)Math.Sin(current_angle * Math.PI / 180);
 
+        //If the W key is held down the seeker travels forward
         if (Input.GetKey(KeyCode.W))
         {
-            rb.velocity = new Vector3(speed * xDir, rb.velocity.y, speed * zDir) * Time.deltaTime * - 1;
+            rb.velocity = new Vector3(movement_speed * xDir, rb.velocity.y, movement_speed * zDir) * Time.deltaTime * - 1;
         }
 
+        //If the S key is held down the seeker travels backwards
         if (Input.GetKey(KeyCode.S))
         {
-            rb.velocity = new Vector3(speed * xDir, rb.velocity.y, speed * zDir) * Time.deltaTime;
+            rb.velocity = new Vector3(movement_speed * xDir, rb.velocity.y, movement_speed * zDir) * Time.deltaTime;
         }
 
+        //If the A key is held the seeker turns to their left
         if (Input.GetKey(KeyCode.A))
         { 
             transform.Rotate(0, - rotation_speed * Time.deltaTime, 0, Space.Self);
             current_angle += rotation_speed * Time.deltaTime;
         }
 
+        //If the D key is held the seeker turns to their right
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(0, rotation_speed * Time.deltaTime, 0, Space.Self);
             current_angle -= rotation_speed * Time.deltaTime;
         }
 
+        //Updates the guide's point of view in the x and z directions to be directly over the seeker
         guide_view.transform.position = new Vector3(transform.position.x, guide_view.transform.position.y, transform.position.z);
     }
 }
