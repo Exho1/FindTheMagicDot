85da2265d99a27c65c90c6bbac21bb5256d8a5e5
GraysonHerbert <65082415+GraysonHerbert@users.noreply.github.com>
2020-11-19 15:50:07 -0600

    Implemented Scanner
    Scanner now is in place. It is a blinking light that blinks faster the closer the seeker is to the object.

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -12,6 +12,12 @@ public class Movement : MonoBehaviour
     Rigidbody rb; //Holds the seeker object
     float current_angle; //The current angle the Seeker is facing from the negative x axis
     public GameObject guide_view; //Holds the guide camera
+    GameObject scanner;
+    GameObject magic_dot;
+    bool scanner_light_on;
+    int scanner_blink_rate;
+    int time_elapsed;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,13 +26,20 @@ public class Movement : MonoBehaviour
         current_angle = 0;
         guide_view = GameObject.FindWithTag("Guide");
         gameObject.GetComponent<Renderer>().material.color = Color.green;
-        GameObject.FindWithTag("Finish").GetComponent<Renderer>().material.color = Color.red;
+        scanner_light_on = false;
+        time_elapsed = 0;
+        magic_dot = GameObject.FindWithTag("Finish");
+        magic_dot.GetComponent<Renderer>().material.color = Color.red;
+        scanner = GameObject.FindWithTag("Scanner");
 
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        scanner_blink_rate = (int)get_distance_between(gameObject, magic_dot);
+
         //Multiplier for the speed in the x direciton
         float xDir = (float)Math.Cos(current_angle * Math.PI / 180);
         //Multiplier for the speed in the z direction
@@ -58,6 +71,23 @@ public class Movement : MonoBehaviour
             current_angle -= rotation_speed * Time.deltaTime;
         }
 
+        time_elapsed++;
+        if (time_elapsed >= scanner_blink_rate)
+        {
+            scanner_light_on = !scanner_light_on;
+            time_elapsed = 0;
+        }
+        
+        if(scanner_light_on)
+        {
+            scanner.GetComponent<Renderer>().material.color = Color.green;
+         
+        }
+        else
+        {
+            scanner.GetComponent<Renderer>().material.color = Color.black;
+        }
+        
         //Updates the guide's point of view in the x and z directions to be directly over the seeker
         guide_view.transform.position = new Vector3(transform.position.x, guide_view.transform.position.y, transform.position.z);
     }
@@ -70,5 +100,15 @@ public class Movement : MonoBehaviour
         {
             Destroy(collision.gameObject);
         }
+    }
+
+    float get_distance_between(GameObject ob1, GameObject ob2)
+    {
+        return (float)Math.Sqrt(Math.Pow(ob1.transform.position.x - ob2.transform.position.x, 2) + Math.Pow(ob2.transform.position.z - ob1.transform.position.z, 2));
+    }
+
+    float get_angle_between(GameObject ob1, GameObject ob2)
+    {
+        return (float)Math.Atan((ob1.transform.position.z - ob2.transform.position.z) / (ob1.transform.position.x - ob2.transform.position.x));
     }
 }
